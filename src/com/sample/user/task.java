ef248d60fbb17a2dfaaeaa3f4778d79934a6b3ac
hp <hp@pc>
2019-08-26 21:19:23 +0530

    Task-1-26-08-2019

---
+++ src/com/sample/user/task.java
@@ -0,0 +1,12 @@
+package com.sample.user;
+import com.sample1.person.*;
+
+
+public class task {
+	public static void main(String[]args) {
+		cube c1=new cube();
+		c1.cubeArea(20);
+		System.out.println("task is ompleted");
+	}
+
+}
